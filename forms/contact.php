07991eb3d2062fb1a65888bca14452a253b11d79
christianloya <christianloya13@yahoo.com>
2022-02-02 09:40:18 -0600

    Added Sanlo emails to distribution

--- forms/contact.php
+++ forms/contact.php
@@ -36,7 +36,7 @@
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
   $contact->add_message( $_POST['message'], 'Message', 10);
-  $contact->cc = array('christianloya@sanlo.tech', 'christianloya13@yahoo.com');
+  $contact->cc = array('christianloya@sanlo.tech', 'timsanchez@sanlo.tech);
 
   echo $contact->send();
 
